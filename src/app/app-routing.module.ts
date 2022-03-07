a5767fdfba425a8301c920ce1c624f51c6dc78fc
andrei-agdo <andrei.ifsp@gmail.com>
2022-03-07 13:04:26 -0300

    commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,35 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'page1',
+    loadChildren: () =>
+      import('./page1/page1.module').then((m) => m.Page1Module),
+    outlet: 'page1',
+  },
+  {
+    path: 'page1',
+    loadChildren: () =>
+      import('./page1/page1.module').then((m) => m.Page1Module),
+    outlet: 'page2',
+  },
+  {
+    path: 'page2',
+    loadChildren: () =>
+      import('./page2/page2.module').then((m) => m.Page2Module),
+    outlet: 'page2',
+  },
+  {
+    path: 'page2',
+    loadChildren: () =>
+      import('./page2/page2.module').then((m) => m.Page2Module),
+    outlet: 'page1',
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,38 @@
 import { Component } from '@angular/core';
+import { ActivatedRoute, Route, Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'aplicacao-teste';
+
+  constructor(private router: Router, private route: ActivatedRoute) {}
+
+  gotoPage1() {
+    this.router.navigate([{ outlets: { page1: ['page1'] } }], {
+      relativeTo: this.route.parent,
+      skipLocationChange: true,
+    });
+  }
+  gotoPage2() {
+    this.router.navigate([{ outlets: { page2: ['page2'] } }], {
+      relativeTo: this.route.parent,
+      skipLocationChange: true,
+    });
+  }
+  gotoPage3() {
+    this.router.navigate([{ outlets: { page1: ['page2'] } }], {
+      relativeTo: this.route.parent,
+      skipLocationChange: true,
+    });
+  }
+  gotoPage4() {
+    this.router.navigate([{ outlets: { page2: ['page1'] } }], {
+      relativeTo: this.route.parent,
+      skipLocationChange: true,
+    });
+  }
 }
